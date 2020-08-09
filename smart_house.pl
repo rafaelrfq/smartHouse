5a52efe8259aa4cb98e39d7fc5c5a80c74c2715d
Rafael <rafaelrfq@gmail.com>
2020-08-09 18:49:53 -0400

    Added cambios gabriel

--- smart_house.pl
+++ smart_house.pl
@@ -3,9 +3,11 @@
 % hechos dinamicos
 :-dynamic dispositivo/1.
 :-dynamic lugar/1.
+:-dynamic lugar/3.
 :-dynamic dispositivo_lugar/2.
-:-dynamic consumo/2.
+:-dynamic consumo/3.
 :-dynamic ubicacion/2.
+:-dynamic estado/3.
 
 % Definicion de hecho dispositivo para decir los que existen en diferentes lugares de la casa
 
@@ -16,9 +18,30 @@ resp_disp(si, X):- !, assertz(dispositivos(X)).
 resp_disp(no, _):- !, write('No se acepta ese dispositivo en esa habitacion.'), fail.
 
 agregar_dispositivo(X):- \+ (dispositivo(X)), assertz(dispositivo(X)).
-agregar_lugar(X):- \+ (lugar(X)), assertz(lugar(X)).
-agregar_disp_lugar(Disp, Lugar):- dispositivo(Disp), lugar(Lugar),
-                        \+ (dispositivo_lugar(Disp, Lugar)), assertz(dispositivo_lugar(Disp, Lugar)).
+% agregar_lugar(X):- \+ (lugar(X)), assertz(lugar(X)).
+% agregar_disp_lugar(Disp, Lugar):- dispositivo(Disp), lugar(Lugar),
+%                         \+ (dispositivo_lugar(Disp, Lugar)), assertz(dispositivo_lugar(Disp, Lugar)).
+
+add_tail([],X,[X]).
+add_tail([H|T],X,[H|L]):-add_tail(T,X,L).
+
+agregar_lugar(Lugar,Tipo):-
+    tipo(Tipo),
+    retractall(lugar(Lugar,,)),
+    assertz(lugar(Lugar, Tipo, [])).
+
+agregar_disp_lugar(Disp, Lugar):-
+    lugar(Lugar,T,L),
+    add_tail(L,Disp, Lista),
+    retract(lugar(Lugar,,)),
+    asserta(lugar(Lugar,T,Lista)).
+
+
+tipo(habitacion).
+tipo(sala).
+tipo(exterior).
+tipo(cocina).
+tipo(comedor).
 
 % Definicion de hecho lugar para decir los lugares que existen en la casa en caso que no se 
 % inserte uno por uno
@@ -59,7 +82,16 @@ agregar_disp_lugar(Disp, Lugar):- dispositivo(Disp), lugar(Lugar),
 % Para poder verificar el uso/consumo de algun dispositivo se necesita un hecho dinamico
 % Prototipo dinamico: consumo(<cantidad>, <dispositivo>).
 
-insertar_consumo(Consumo, Dispositivo):- dispositivo(Dispositivo), retract(consumo(_, Dispositivo)), assertz(consumo(Consumo, Dispositivo)).
+insertar_consumo(Consumo, Dispositivo, Lugar, Resultado):- dispositivo_lugar(Dispositivo, Lugar), retract(consumo(_, Dispositivo, Lugar)), 
+                                                           assertz(consumo(Consumo, Dispositivo, Lugar)), verificar_consumo(Consumo, Dispositivo, Lugar, Resultado), !.
+% Si no existe el consumo, se hace esto:
+insertar_consumo(Consumo, Dispositivo, Lugar, Resultado):- dispositivo_lugar(Dispositivo, Lugar), assertz(consumo(Consumo, Dispositivo, Lugar)),
+                                                           verificar_consumo(Consumo, Dispositivo, Lugar, Resultado).
+
+verificar_consumo(regular, _, _):- write('Se mantiene el mismo estado en el dispositivo'), !.
+verificar_consumo(alto, Dispositivo, Lugar, Resultado):- retract(estado(Dispositivo, Lugar, _)), assertz(estado(Dispositivo, Lugar, 0)), 
+                                                         Resultado = 'Se apago el dispositivo para eficientizar los recursos.'.
+verificar_consumo(alto, Dispositivo, Lugar, Resultado):- assertz(estado(Dispositivo, Lugar, 0)), Resultado = 'Se apago el dispositivo para eficientizar los recursos.'.
 
 % Para tomar acciones en base a consumo se usaran las siguientes reglas
 % ---
